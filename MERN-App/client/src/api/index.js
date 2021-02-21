8a9ead4bde8743f260b042f33916f9aea0a801e4
Jerry <jcotellessa@aol.com>
2021-02-21 12:38:00 -0500

    Added MySQL connection and changes to the Client and Server Index files

--- MERN-App/client/src/api/index.js
+++ MERN-App/client/src/api/index.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:3001/api"
-  baseURL: "http://192.168.0.106:3001/api"
+  baseURL: "http://localhost:3001/api"
+  //baseURL: "http://192.168.0.106:3001/api"
 });
 
 export const insertScore = payload => api.post(`/score`, payload);
 export const getAllScores = () => api.get(`/scores`);
-export const updateScoreById = (id, payload) => {
-  api.put(`/score/${id}`, payload);
-};
+export const updateScoreById = (id, payload) => { api.put(`/score/${id}`, payload); };
 export const deleteScoreById = id => api.delete(`/score/${id}`);
 export const getScoreById = id => api.get(`/score/${id}`);
 
