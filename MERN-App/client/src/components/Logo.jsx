1fadb550a23dc1786a39daa78f87adb7a115ae09
Jerry <jcotellessa@aol.com>
2021-02-22 21:48:03 -0500

    Updated the Logo link to CNN.com and removed the Dash in eLink

--- MERN-App/client/src/components/Logo.jsx
+++ MERN-App/client/src/components/Logo.jsx
@@ -10,7 +10,7 @@ const Wrapper = styled.a.attrs({
 class Logo extends Component {
     render() {
         return (
-            <Wrapper href="https://boston.com">
+            <Wrapper href="https://cnn.com">
                 <img src={logo} width="50" height="50" alt="samboros.com" />
             </Wrapper>
         )
