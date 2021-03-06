ea182e4299cc8d30a62f709b0ae74f06491cff4c
jerry <jcotellessa@aol.com>
2021-03-06 09:55:40 -0500

    adds an initial test to be expanded upon

--- MERN-App/client/src/api/index.js
+++ MERN-App/client/src/api/index.js
@@ -1,23 +1,24 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api"
+  //  baseURL: "http://localhost:3000/api"
+  baseURL: "http://10.242.1.119:3000/api",
 });
 
-export const insertScore = payload => api.post(`/score`, payload);
+export const insertScore = (payload) => api.post(`/score`, payload);
 export const getAllScores = () => api.get(`/scores`);
 export const updateScoreById = (id, payload) => {
   api.put(`/score/${id}`, payload);
 };
-export const deleteScoreById = id => api.delete(`/score/${id}`);
-export const getScoreById = id => api.get(`/score/${id}`);
+export const deleteScoreById = (id) => api.delete(`/score/${id}`);
+export const getScoreById = (id) => api.get(`/score/${id}`);
 
 const apis = {
   insertScore,
   getAllScores,
   updateScoreById,
   deleteScoreById,
-  getScoreById
+  getScoreById,
 };
 
 export default apis;
